f2ae5e1e31164da6b58cd5bb8f351813f1fe5df8
Christopher Rudmin <chris.rudmin@gmail.com>
2021-10-05 22:38:03 -0400

    Add test

--- src/CardGame/CardGame.test.js
+++ src/CardGame/CardGame.test.js
@@ -1,8 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from '@testing-library/react';
+import CardGame from './CardGame';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('should render', () => {
+  const { asFragment } = render(<CardGame />);
+  expect(asFragment()).toMatchSnapshot();
 });
